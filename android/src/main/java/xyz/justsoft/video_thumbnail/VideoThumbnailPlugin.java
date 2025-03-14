f2e35669867ff2a0a5a26e0a9576841f87e41c2b
Sátor Viktor <sator.viktor@amarone.hu>
2025-03-14 14:36:03 +0100

    uses or overrides deprecated API issue

--- android/src/main/java/xyz/justsoft/video_thumbnail/VideoThumbnailPlugin.java
+++ android/src/main/java/xyz/justsoft/video_thumbnail/VideoThumbnailPlugin.java
@@ -100,7 +100,7 @@ public class VideoThumbnailPlugin implements FlutterPlugin, MethodCallHandler {
             case 1:
                 return Bitmap.CompressFormat.PNG;
             case 2:
-                return Bitmap.CompressFormat.WEBP;
+                return Build.VERSION.SDK_INT > 30 ? Bitmap.CompressFormat.WEBP_LOSSLESS : Bitmap.CompressFormat.WEBP;
         }
     }
 
